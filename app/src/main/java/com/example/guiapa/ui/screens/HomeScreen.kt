4f502c1bbc8bd8107bcaf3317a3cf6e534c12bdc
Gabriel432135 <gabriel432135@hotmail.com>
2026-03-10 20:04:37 -0300

    Scaffold

--- app/src/main/java/com/example/guiapa/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/guiapa/ui/screens/HomeScreen.kt
@@ -1,16 +1,24 @@
 package com.example.guiapa.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(){
     Scaffold(
-        
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Guia PA")
+                }
+            )
+        }
     )
     {
         innerPadding ->
